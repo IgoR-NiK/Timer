4306b084b5f7065a2a6259c117883803ef2fd4b8
Den <Igor_2501@mail.ru>
2018-08-29 21:22:59 +0300

    Поддержка склонений после числительных

---
+++ Timer/Helpers/DeclensionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.Helpers
+{
+    static class DeclensionHelper
+    {
+        /// <summary>
+        /// Возвращает слова в падеже, зависимом от заданного числа.
+        /// </summary>
+        /// <param name="number">Число от которого зависит выбранное слово.</param>
+        /// <param name="nominativ">Именительный падеж слова. Например "день".</param>
+        /// <param name="genetiv">Родительный падеж слова. Например "дня".</param>
+        /// <param name="plural">Множественное число слова. Например "дней".</param>
+        /// <returns></returns>
+        public static string GetDeclension(int number, string nominativ, string genetiv, string plural)
+        {
+            number %= 100;
+            if (number >= 11 && number <= 19)
+            {
+                return plural;
+            }
+
+            number %= 10;
+            switch (number)
+            {
+                case 1:
+                    return nominativ;
+                case 2:
+                case 3:
+                case 4:
+                    return genetiv;
+                default:
+                    return plural;
+            }
+        }
+    }
+}
--- Timer/ViewModels/CountDownVM.cs
+++ Timer/ViewModels/CountDownVM.cs
@@ -5,21 +5,24 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+
+using Timer.Helpers;
 
 namespace Timer.ViewModels
 {
     class CountDownVM : INotifyPropertyChanged
     {
-        DateTime EndDate { get; } = new DateTime(2018, 8, 30);
+        DateTime EndDate { get; } = new DateTime(2018, 8, 31);
 
         public CountDownVM()
         {
             Title = "До защиты диплома осталось:";
 
-            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(500000);
-            dispatcherTimer.Start();
+            var timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = new TimeSpan(500000);
+            timer.Start();
         }
 
         private string title;
@@ -44,7 +47,8 @@ namespace Timer.ViewModels
             {
                 days = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(DaysVisible));                
+                OnPropertyChanged(nameof(DaysVisible));
+                OnPropertyChanged(nameof(DaysLabel));
             }
         }
         
@@ -57,6 +61,7 @@ namespace Timer.ViewModels
                 hours = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(HoursVisible));
+                OnPropertyChanged(nameof(HoursLabel));
             }
         }
 
@@ -68,7 +73,8 @@ namespace Timer.ViewModels
             {
                 minutes = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(MinutesVisible));                
+                OnPropertyChanged(nameof(MinutesVisible));
+                OnPropertyChanged(nameof(MinutesLabel));
             }
         }
 
@@ -81,6 +87,7 @@ namespace Timer.ViewModels
                 seconds = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(SecondsVisible));
+                OnPropertyChanged(nameof(SecondsLabel));
             }
         }
 
@@ -89,7 +96,12 @@ namespace Timer.ViewModels
         public bool MinutesVisible => Days != 0 || Hours != 0 || Minutes != 0;
         public bool SecondsVisible => Days != 0 || Hours != 0 || Minutes != 0 || Seconds != 0;
 
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        public string DaysLabel => DeclensionHelper.GetDeclension(Days, "День", "Дня", "Дней");
+        public string HoursLabel => DeclensionHelper.GetDeclension(Hours, "Час", "Часа", "Часов");
+        public string MinutesLabel => DeclensionHelper.GetDeclension(Minutes, "Минута", "Минуты", "Минут");
+        public string SecondsLabel => DeclensionHelper.GetDeclension(Seconds, "Секунда", "Секунды", "Секунд");
+
+        private void timer_Tick(object sender, EventArgs e)
         {
             var currentDate = DateTime.Now;
             var delta = EndDate - currentDate;
